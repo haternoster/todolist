f9da76280d61d15bb68d19d11275e7c6169d731d
Petr <haternoster@icloud.com>
2023-10-28 13:50:22 +0200

    saving as csv file

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+import csv
+import os.path
+
 if __name__ == "__main__":
 
     notes = []
+    notesfile = "notes.csv"
+
+    def is_file_exists(filename):
+        return os.path.isfile(filename)
 
 
+    def load_from_file(filename):
+        if not is_file_exists(filename):
+            return []
+        with open(filename, newline='') as csvfile:
+            notesreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            notes = []
+            for note in notesreader:
+                notes += [note[0]]
+        return notes
+
+    def save_to_file(filename):
+        with open(filename, 'w', newline='') as csvfile:
+            noteswriter = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for i in notes:
+                noteswriter.writerow([i])
+
+
+    notes = load_from_file(notesfile)
     while True:
+
         print("> ", end = "")
         command = input()
         if command == "q":
@@ -21,14 +48,17 @@ if __name__ == "__main__":
                 if index != (i+1):
                     new_notes += [notes[i]]
             notes = new_notes
+            save_to_file(notesfile)
             print("removed")
             continue
         if command == "add":
             note = input("note: ")
             notes += [note]
+            save_to_file(notesfile)
             print("added")
             continue
         if command == "remove_last":
+            save_to_file(notesfile)
             print("last removed")
             continue
         if command == "help":
